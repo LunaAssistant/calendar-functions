e2c4f268f65473f8a17476c99d458ea18d6f30a4
mauriciotntx <andresmaopinzon@gmail.com>
2021-02-20 01:36:26 -0600

    Delete events

--- functions/src/repositories/EventsRepository.js
+++ functions/src/repositories/EventsRepository.js
@@ -2,28 +2,49 @@ const admin = require("firebase-admin");
 const moment = require("moment");
 
 class EventsRepository {
-  saveEvents(uid, events) {
-    // Get a new write batch
-    var batch = admin.firestore().batch();
+    saveEvents(uid, events) {
+        // Get a new write batch
+        const batch = admin.firestore().batch();
 
-    events.forEach((event) => {
-      var eventRef = admin
-        .firestore()
-        .collection(`users/${uid}/events`)
-        .doc(event.id);
+        events.forEach((event) => {
+            var eventRef = admin
+                .firestore()
+                .collection(`users/${uid}/events`)
+                .doc(event.id);
 
-      event.startsAt = moment(event.start.dateTime);
-      event.endsAt = moment(event.end.dateTime);
+            event.startsAt = moment(event.start.dateTime);
+            event.endsAt = moment(event.end.dateTime);
 
-      batch.set(eventRef, event);
-    });
+            batch.set(eventRef, event);
+        });
 
-    // Commit the batch
-    return batch.commit().then(() => {
-      console.log("Updated events: ", events.length);
-      return true;
-    });
-  }
+        // Commit the batch
+        return batch.commit().then(() => {
+            console.log("Updated events: ", events.length);
+            return true;
+        });
+    }
+
+    async deleteEvents(uid, start, end) {
+        const batch = await admin
+            .firestore().collection(`users/${uid}/events`)
+            .where("startsAt", ">=", start)
+            .where("startsAt", "<=", end)
+            .get()
+            .then((querySnapshot) => {
+                const batch = admin.firestore().batch();
+
+                querySnapshot.forEach((doc) => {
+                    batch.delete(doc.ref);
+                });
+
+                return batch
+            })
+
+        return batch.commit().catch((error) => {
+            console.error("Error removing documents: ", error);
+        });
+    }
 }
 
 exports.EventsRepository = EventsRepository;
--- functions/src/services/EventsService.js
+++ functions/src/services/EventsService.js
@@ -1,97 +1,102 @@
-const { EventsRepository } = require("./../repositories/EventsRepository.js");
+const {EventsRepository} = require("./../repositories/EventsRepository.js");
 const moment = require("moment-timezone");
 
 class EventsService {
-  getEvents(calendar, data) {
-    return calendar.events
-      .list({
-        calendarId: data.calendarName ? data.calendarName : "primary",
-        timeMin: data.start,
-        timeMax: data.end,
-        maxResults: data.limit,
-        singleEvents: true,
-        orderBy: "startTime",
-        timeZone: data.timeZone,
-      })
-      .then((response) => {
-        return response.data.items;
-      })
-      .then((events) => {
-        return {
-          events: events.map((event) => {
-            return {
-              ...event,
-              self: event.attendees
-                ? event.attendees.find((attendee) => {
-                    return attendee.self === true;
-                  })
-                : {
-                    self: true,
-                    responseStatus: "accepted",
-                  },
-            };
-          }),
-        };
-      })
-      .catch((error) => {
-        console.error("Error getting events from API: ", error);
-        throw error;
-      });
-  }
+    getEvents(calendar, data) {
+        return calendar.events
+            .list({
+                calendarId: data.calendarName ? data.calendarName : "primary",
+                timeMin: data.start,
+                timeMax: data.end,
+                maxResults: data.limit,
+                singleEvents: true,
+                orderBy: "startTime",
+                timeZone: data.timeZone,
+            })
+            .then((response) => {
+                return response.data.items;
+            })
+            .then((events) => {
+                return {
+                    events: events.map((event) => {
+                        return {
+                            ...event,
+                            self: event.attendees
+                                ? event.attendees.find((attendee) => {
+                                    return attendee.self === true;
+                                })
+                                : {
+                                    self: true,
+                                    responseStatus: "accepted",
+                                },
+                        };
+                    }),
+                };
+            })
+            .catch((error) => {
+                console.error("Error getting events from API: ", error);
+                throw error;
+            });
+    }
 
-  createEvent(calendar, calendarId = "primary", eventResource) {
-    return calendar.events
-      .insert({
-        calendarId: calendarId,
-        resource: eventResource,
-      })
-      .then((response) => {
-        return response.data;
-      });
-  }
+    createEvent(calendar, calendarId = "primary", eventResource) {
+        return calendar.events
+            .insert({
+                calendarId: calendarId,
+                resource: eventResource,
+            })
+            .then((response) => {
+                return response.data;
+            });
+    }
 
-  getEvent(calendar, calendarId = "primary", eventId) {
-    return calendar.events
-      .get({
-        calendarId,
-        eventId,
-      })
-      .then((response) => {
-        return response.data;
-      });
-  }
+    getEvent(calendar, calendarId = "primary", eventId) {
+        return calendar.events
+            .get({
+                calendarId,
+                eventId,
+            })
+            .then((response) => {
+                return response.data;
+            });
+    }
 
-  async updateEvent(calendar, calendarId = "primary", eventId, data) {
-    let eventResource = await this.getEvent(calendar, calendarId, eventId);
-    eventResource.colorId = data.colorId;
+    async updateEvent(calendar, calendarId = "primary", eventId, data) {
+        let eventResource = await this.getEvent(calendar, calendarId, eventId);
+        eventResource.colorId = data.colorId;
 
-    return calendar.events
-      .update({
-        calendarId: calendarId ? calendarId : "primary",
-        eventId: eventId,
-        resource: eventResource,
-      })
-      .then((response) => {
-        return response.data;
-      });
-  }
+        return calendar.events
+            .update({
+                calendarId: calendarId ? calendarId : "primary",
+                eventId: eventId,
+                resource: eventResource,
+            })
+            .then((response) => {
+                return response.data;
+            });
+    }
 
-  async refreshToday(calendar, uid, tz) {
-    const eventsRepository = new EventsRepository();
+    async refreshToday(calendar, uid, tz) {
+        const eventsRepository = new EventsRepository();
+        const start = moment().tz(tz).startOf("day")
+        const end = moment().tz(tz).endOf("day")
 
-    console.log(moment().tz(tz));
+        console.log(moment().tz(tz));
 
-    return this.getEvents(calendar, {
-      start: moment().tz(tz).startOf("day").format(),
-      end: moment().tz(tz).endOf("day").format(),
-      limit: 500,
-      singleEvents: true,
-      orderBy: "startTime",
-      timeZone: tz,
-    }).then((response) => {
-      return eventsRepository.saveEvents(uid, response.events);
-    });
-  }
+        return Promise.all([
+            eventsRepository.deleteEvents(uid, start.toDate(), end.toDate()),
+            this.getEvents(calendar, {
+                start: start.format(),
+                end: end.format(),
+                limit: 500,
+                singleEvents: true,
+                orderBy: "startTime",
+                timeZone: tz,
+            })
+        ]).then((responses) => {
+            return eventsRepository.saveEvents(uid, responses[1].events);
+        });
+    }
 }
 
 exports.EventsService = EventsService;
